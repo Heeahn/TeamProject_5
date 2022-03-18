da26bd9f3587ba71624662ffde272ea2b29ff07a
504 <504@DESKTOP-5GM5QKH>
2022-03-18 16:34:58 +0900

    a

---
+++ TeamProject_5/src/PMW/Enter.java
@@ -0,0 +1,14 @@
+package PMW;
+
+import java.util.Scanner;
+
+public class Enter {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		
+		System.out.println(i);
+		
+	}
+}
