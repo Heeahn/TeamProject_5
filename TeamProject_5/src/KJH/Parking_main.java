6de6aed3f33b8e7be000c48fe5c4a2af61c89a2e
504 <504@DESKTOP-M91NLNL>
2022-03-21 14:25:18 +0900

    main update

--- TeamProject_5/src/KJH/Parking_main.java
+++ TeamProject_5/src/KJH/Parking_main.java
@@ -43,7 +43,7 @@ public class Parking_main {
 		
 		/*	주의 사항
 		 * 	Parking_In   ---
-		 *  Parking_exit   |----  에  fileInput,fileOutput 용  txt file 생성 및 경로지정해야함   
+		 *  Parking_exit	|----  에  fileInput,fileOutput 용  txt file 생성 및 경로지정해야함   
 		 * 	Tower_money  ---   
 		 * 
 		 * Parking_In.java : FileOutputStream outputStream = new FileOutputStream("D:/java/차량입출차.txt" , true );
